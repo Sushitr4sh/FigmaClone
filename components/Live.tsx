363be3b84b0ec51847301ec5b5c141951ff1d089
Mario Imanuel Daruranto <mariodaruranto68@gmail.com>
2025-01-20 23:50:21 +0700

    feat: add live cursors

---
+++ components/Live.tsx
@@ -0,0 +1,54 @@
+import React, { useCallback, useState } from "react";
+import LiveCursors from "./cursor/LiveCursors";
+import { useMyPresence, useOthers } from "@liveblocks/react/suspense";
+import CursorChat from "./cursor/CursorChat";
+import { CursorMode } from "@/types/type";
+
+const Live = () => {
+  const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const [cursorState, setCursorState] = useState({ mode: CursorMode.Hidden });
+
+  //call back so it doesn't recreate this function every time, it just take on instace of the function inside and then provides the output
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  //hide cursor once we left the screen
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+
+    updateMyPresence({ cursor: null, message: null });
+  }, []);
+
+  //canvas functionality
+  const handlePointerDown = useCallback((event: React.PointerEvent) => {
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  return (
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className="h-screen w-full flex justify-center items-center text-center"
+    >
+      <h1 className="text-2xl text-white ">Liveblocks Figma Clone</h1>
+
+      {cursor && <CursorChat cursor={cursor} />}
+
+      <LiveCursors others={others} />
+    </div>
+  );
+};
+
+export default Live;
